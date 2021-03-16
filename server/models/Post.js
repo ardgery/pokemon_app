e1d1a91cb93f65e2dd82b98e9331224db3df67d6
ardgery <ardinegery@gmail.com>
2021-03-16 21:06:59 +0700

    Latest merng stacks

--- server/models/Post.js
+++ server/models/Post.js
@@ -16,11 +16,7 @@ const postSchema = new Schema({
             username: String,
             createdAt: String
         }
-    ],
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-    }
+    ]
 })
 
 module.exports = model('Post', postSchema)
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 // const schema = require('./schema');
 
@@ -6,10 +6,12 @@ const { MONGODB } = require('./config');
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({ req }) => ({ req, pubsub })
 });
 
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
